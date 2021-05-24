f9f466c5ce8bdd4a8c7bf9a2b912da4d20e4b502
joao <joaovlima07@hotmail.com>
2021-05-24 11:57:30 -0300

    html/css almost done

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
-var types = {type: [{num: 3},{num:4}]}
+
+//vars
+ let buttonLotofacil = document.querySelector('#lotofacil')
+ let buttonMega = document.querySelector('#mega')
+ let buttonLotomania = document.querySelector('#lotomania')
+
+ let descriptionGame = document.querySelector('#gameDescription')
+
+ let numbersDiv = document.querySelector('#numbers')
+
+ let setButton = ''
+
+ let numbersArray = []
+
+ //get description
+ buttonLotofacil.addEventListener('click', ()=>{
+    numbersDiv.innerText = ''
+
+     fetch('games.json', {method: 'GET'}).then((res)=>{return res.json()})
+     .then((data)=>{
+         descriptionGame.innerHTML = data.types[0].description
+
+
+        for(let i = 1; i <= data.types[0].range; i++){
+            const divInside = document.createElement('button')
+            divInside.classList.add('myNumber')
+            divInside.style.background = `${data.types[0].color}`
+            divInside.innerHTML = i;
+
+            numbersArray.push(divInside)
+
+            numbersDiv.appendChild(divInside)
+        }
+
+        return setButton = 'lotofacil'
+     })
+ })
+
+ buttonMega.addEventListener('click', ()=>{
+    numbersDiv.innerText = ''
+
+    fetch('games.json', {method: 'GET'}).then((res)=>{return res.json()})
+    .then((data)=>{
+        
+        descriptionGame.innerHTML = data.types[1].description
+
+        for(let i = 1; i <= data.types[1].range; i++){
+            const divInside = document.createElement('button')
+            divInside.classList.add('myNumber')
+            divInside.style.background = `${data.types[1].color}`
+            divInside.innerHTML = i;
+            
+            numbersArray.push(divInside)
+
+            numbersDiv.appendChild(divInside)
+        }
+        return setButton = 'mega'
+    })
+})
+
+buttonLotomania.addEventListener('click', ()=>{
+    numbersDiv.innerText = ''
+    
+    fetch('games.json', {method: 'GET'}).then((res)=>{return res.json()})
+    .then((data)=>{
+        descriptionGame.innerHTML = data.types[2].description
+
+        for(let i = 1; i <= data.types[2].range; i++){
+            const divInside = document.createElement('button')
+            divInside.classList.add('myNumber')
+            divInside.style.background = `${data.types[2].color}`
+            divInside.innerHTML = i;
+
+            numbersArray.push(divInside)
+
+            numbersDiv.appendChild(divInside)
+        }
+
+        return setButton = 'lotomania'
+    })
+})
